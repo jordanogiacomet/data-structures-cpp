959a06f1e490e324993736f8958d2064b69c2a1b
Jordano <jgtomazoni@ucs.br>
2025-03-12 18:09:25 -0300

    WIP - Evaluate prefix expression using stack

---
+++ evaluate_prefix_expression_using_stack.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stack>
+#include <string.h>
+using namespace std;
+
+int EvaluatePrefix(char *exp);
+int Perform(char exp, int op1, int op2);
+
+int main(void)
+{
+    const int MAX = 100;
+    char expression[MAX];
+    cout << "Enter a expression: ";
+    cin.getline(expression, MAX);
+
+    int res = EvaluatePrefix(expression);
+
+    cout << "The result is: " << res << "\n";
+}
+
+int Perform(char exp, int op1, int op2)
+{
+    switch (exp)
+    {
+    case '+':
+        return op1 + op2;
+    case '-':
+        return op1 - op2;
+    case '*':
+        return op1 * op2;
+    case '/':
+        if (op2 == 0)
+        {
+            fprintf(stderr, "Erro: Divisão por zero.\n");
+            exit(EXIT_FAILURE);
+        }
+        return op1 / op2;
+    default:
+        fprintf(stderr, "Erro: Operador '%c' inválido.\n", exp);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int EvaluatePrefix(char *exp)
+{   
+    int res;
+    stack<int> S;
+    int length = strlen(exp);
+    for(int i = length - 1; i >= 0; i--)
+    {
+        if(exp[i] == ' ')
+        {
+            continue;
+        }
+        if(isdigit(exp[i]))
+        {
+            S.push(exp[i] - '0');
+        }
+        else
+        {
+            int op1 = S.top(); S.pop();
+            int op2 = S.top(); S.pop();
+            res = Perform(exp[i], op1, op2);
+            S.push(res);
+        }
+    }
+    return S.top();
+}
